71288df693579025bc0e102c55152e41a558268b
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-05-15 16:49:34 +0300

    lists

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+
+    if length == 0:
+      print(fib_sequence)
+      return
+   
+    fib_sequence.append(0)
+
+    if length > 1:
+      fib_sequence.append(1)
+
+    while len(fib_sequence) < length:
+         next_number = fib_sequence[-1] + fib_sequence[-2]
+         fib_sequence.append(next_number)
+
+
+    print(fib_sequence)
+      
